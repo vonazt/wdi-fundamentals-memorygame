83ff4e63e280cbfed20675a99d8376a815ffe1da
Richard Tzanov <richardtzanov@gmail.com>
2018-01-19 12:41:16 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log('User just flipped ' + cardsInPlay[0]);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log('User just flipped ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	} else {
-		alert('Sorry,try again');
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!');
+		} else {
+			alert('Sorry,try again');
+		}
 	}
-}
+};
+
+var flipCard = function(cardId) {
+	console.log('User just flipped ' + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
 
 
 
